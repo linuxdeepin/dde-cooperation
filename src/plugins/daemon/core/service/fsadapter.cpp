9be81ed35061bc4408808afb52496926f7928324
liyigang <liyigang1@uniontech.com>
2023-12-05 18:01:33 +0800

    feat: file trans modify
    file trans modify
    
    Log: file trans modify

--- src/plugins/daemon/core/service/fsadapter.cpp
+++ src/plugins/daemon/core/service/fsadapter.cpp
@@ -6,6 +6,7 @@
 #include "message.pb.h"
 #include "utils/utils.h"
 #include "utils/config.h"
+#include "common/constant.h"
 
 #include "co/log.h"
 #include "co/path.h"
@@ -75,7 +76,6 @@ bool FSAdapter::newFileByFullPath(const char *fullpath, bool isdir)
             fx.close();
         }
     }
-//    LOG << "new file -> fullpath: " << fullpath;
 
     return fs::exists(fullpath);
 }
@@ -102,6 +102,8 @@ bool FSAdapter::noneExitFileByFullPath(const char *fullpath, bool isdir, fastrin
 
 bool FSAdapter::writeBlock(const char *name, int64 seek_len, const char *data, size_t size)
 {
+    if (size <= 0)
+        return true;
     fs::file fx(name, 'm');
     if (!fx.exists()) {
         ELOG << "writeBlock File does not exist: " << name;
@@ -123,6 +125,60 @@ bool FSAdapter::writeBlock(const char *name, int64 seek_len, const char *data, s
     fx.close();
 
     return true;
+}
+
+bool FSAdapter::writeBlock(const char *name, int64 seek_len,
+                           const char *data, size_t size, const int flags, fs::file **fx)
+{
+    if (flags & JobTransFileOp::FIlE_CREATE) {
+        if ((*fx) != nullptr) {
+            // 这里创建文件发现文件描述符存在，错误返回 -1
+            ELOG << "file flags is create, but file fx is not nullptr, flags = " << flags;
+            (*fx)->close();
+            delete (*fx);
+            (*fx) = nullptr;
+            return false;
+        }
+        fastring parent = path::dir(name);
+        fs::mkdir(parent, true); // 创建文件保存的根/子目录
+        (*fx) = new fs::file(name, 'm');
+        if (!(*fx)->exists()) {
+            ELOG << " file create error , file = " << name << ", flags = " << flags;
+            (*fx)->close();
+            delete (*fx);
+            (*fx) = nullptr;
+            return false;
+        }
+    }
+
+    if ((*fx) == nullptr) {
+        ELOG << "fx is nullptr !!!!!! " << name << " flags = " << flags << " len " << size;
+        return false;
+    }
+    bool write = true;
+    if (size != 0) {
+        size_t wirted_size = 0;
+        size_t rem_size = size;
+        (*fx)->seek(seek_len);
+        do {
+            size_t wsize = (*fx)->write(data, rem_size);
+            if (wsize <= 0) {
+                write = false;
+                ELOG << "fx write done: " << rem_size << " => " << wsize;
+                break;
+            }
+            wirted_size += wsize;
+            rem_size = size - wsize;
+        } while (wirted_size < size);
+    }
+
+    if (flags & JobTransFileOp::FILE_CLOSE || !write) {
+        (*fx)->close();
+        delete (*fx);
+        (*fx) = nullptr;
+    }
+
+    return write;
 }
 
 fastring FSAdapter::noneExitPath(const char *name)
